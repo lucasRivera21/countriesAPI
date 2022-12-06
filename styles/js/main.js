21f204faf78209ab0ccebb3f5bcfc83a89c50cda
lucasRivera21 <lucasriverar2001@gmail.com>
2022-12-05 22:15:39 -0500

    first version

---
+++ styles/js/main.js
@@ -0,0 +1,16 @@
+const darkMode = document.querySelector('.dark-mode-btn');
+let mode = true;
+let color = document.documentElement.style;
+darkMode.addEventListener('click', () =>{
+    if(mode){
+        color.setProperty('--background-color-white', '#2B3844');
+        color.setProperty('--color-text', '#FFFFFF');
+        color.setProperty('--background-color-body', '#202C36');
+        mode = false;
+    }else{
+        color.setProperty('--background-color-white', '#FFFFFF');
+        color.setProperty('--color-text', '#111517');
+        color.setProperty('--background-color-body', '#FAFAFA');
+        mode = true;
+    };
+});
